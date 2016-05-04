496d0a62558583e3445383709c05e897b6519b53
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-05-04 12:01:17 +0200

    Adding /api request with JSON format

--- app.js
+++ app.js
@@ -5,4 +5,8 @@ app.get('/', function(request, response) {
     response.send("This would be some HTML");
 });
 
+app.get('/api', function(request, response) {
+    response.send({name:"Nacho",age:25});
+});
+
 app.listen(3000);
