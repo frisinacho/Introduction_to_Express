2a3ba858c5d302dacdd44c8cea0d1bb0374d05d1
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-05-10 13:09:20 +0200

    Displaying blog entries on the Home Page

--- app.js
+++ app.js
@@ -3,19 +3,23 @@ var app = express();
  
 var hbs = require('hbs');
  
+var blogEngine = require('./blog');
+ 
 app.set('view engine', 'html');
 app.engine('html', hbs.__express);
+app.use(express.bodyParser());
  
 app.get('/', function(req, res) {
-    res.render('index');
+    res.render('index',{title:"My Blog", entries:blogEngine.getBlogEntries()});
 });
  
 app.get('/about', function(req, res) {
-    res.render('about');
+    res.render('about', {title:"About Me"});
 });
  
-app.get('/article', function(req, res) {
-    res.render('article');
+app.get('/article/:id', function(req, res) {
+    var entry = blogEngine.getBlogEntry(req.params.id);
+    res.render('article',{title:entry.title, blog:entry});
 });
  
 app.listen(3000);
